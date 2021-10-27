2914b71feff3cea2eb999f501332742ec2bb4655
nipunChathura <nipunc1999@gmail.com>
2021-10-28 04:06:06 +0530

    Create item command process

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import sys
+from item import Item
 
 if __name__ == "__main__":
     arguments = sys.argv
-    print("print", arguments)
+
+    section = arguments[1]
+    command = arguments[2]
+    params = arguments[3:]
+    print(section, command, params)
+
+    if section == "user":
+        item = Item()
+        item.set_command_and_params(command, params)
+
